c6d16b739ff373468e0418d68ee7a4c2a86b1089
ismail1997 <bouaddi1997@gmail.com>
2021-09-15 11:40:22 +0100

    new product form

--- src/main/java/com/ismail/products/entities/Product.java
+++ src/main/java/com/ismail/products/entities/Product.java
@@ -9,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Data @ToString @AllArgsConstructor
@@ -16,7 +19,10 @@ import java.io.Serializable;
 public class Product implements Serializable {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min=6,max=70)
     private String designation;
+    @DecimalMin("50")
     private double price;
     private int quantity;
 }
--- src/main/java/com/ismail/products/web/ProductController.java
+++ src/main/java/com/ismail/products/web/ProductController.java
@@ -8,8 +8,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -34,12 +36,16 @@ public class ProductController {
     }
 
     @GetMapping("/formProduct")
-    public String form(){
+    public String form(Model model){
+        model.addAttribute("product",new Product());
         return "formProduct";
     }
 
     @PostMapping("/save")
-    public String save(Product product){
+    public String save(Model model,@Valid Product product, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "formProduct";
+        }
         productRepository.save(product);
         return "formProduct";
     }
